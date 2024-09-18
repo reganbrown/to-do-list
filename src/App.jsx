bc8a713c2a88c1c148ae2b3959239e1e48c5fa6e
reganbrown <regan_brown@icloud.com>
2024-09-18 12:17:12 -0300

    add useState to update list with user input

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-// import { useState } from "react";
+import { useState } from "react";
 import "./App.scss";
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Input from "./components/Input/Input";
 
 function App() {
-  const ItemsToDo = ["Buy stuff", "Return stuff"];
+  let [ItemsToDo, setItemsToDo] = useState(["Buy stuff", "Return stuff"]);
 
   function updateList(item) {
-    console.log(item);
-    ItemsToDo.push(item);
-    console.log(ItemsToDo);
+    const newItemsToDo = [...ItemsToDo, item];
+    setItemsToDo(newItemsToDo);
   }
+
   return (
     <>
       <Header />
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -8,6 +8,7 @@ export default function Input({ push }) {
   return (
     <>
       <form
+        className="toDoForm"
         onSubmit={(event) => {
           submitHandler(event);
         }}
